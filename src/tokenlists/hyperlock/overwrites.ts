93d7dea50ced1ff6055494f700ac606c1d887ca3
0xeye <97349378+0xeye@users.noreply.github.com>
2024-03-07 16:45:41 +0000

    chore: add aura, metastreet

--- src/tokenlists/hyperlock/overwrites.ts
+++ src/tokenlists/hyperlock/overwrites.ts
@@ -2,9 +2,18 @@
 import { Network, OverwritesForList } from '../../types'
 
 export const overwrites: OverwritesForList = {
-  // [Network.Blast]: {
-  //   '0x4300000000000000000000000000000000000004': {
-  //     logoURI: "https://assets.coingecko.com/coins/images/279/standard/ethereum.png"
-  //   },
-  // },
+  [Network.Blast]: {
+    '0x999f220296b5843b2909cc5f8b4204aaca5341d8': {
+      address: "0x999f220296b5843b2909cc5f8b4204aaca5341d8",
+      name: "Metastreet Punks",
+      symbol: "mwsteth:WPUNKS:40",
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/tokens/${'0x999f220296b5843b2909cc5f8b4204aaca5341d8'}.png`
+    },
+    '0x1509706a6c66ca549ff0cb464de88231ddbe213b': {
+      address: "0x1509706a6c66ca549ff0cb464de88231ddbe213b",
+      name: "Aura Finance",
+      symbol: "0x1509706a6c66ca549ff0cb464de88231ddbe213b",
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/tokens/${'0x1509706a6c66ca549ff0cb464de88231ddbe213b'}.png`
+    },
+  },
 }
