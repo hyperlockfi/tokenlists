d0d85ee939e90a7cd75e97f35f148bdf2b84f932
0xeye <97349378+0xeye@users.noreply.github.com>
2024-03-12 13:37:36 +0000

    chore: add token wave

--- src/tokenlists/hyperlock/overwrites.ts
+++ src/tokenlists/hyperlock/overwrites.ts
@@ -22,6 +22,30 @@ export const overwrites: OverwritesForList = {
       symbol: "BAG",
       logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/tokens/${'0xb9dfcd4cf589bb8090569cb52fac1b88dbe4981f'}.png`
     },
+    '0x2416092f143378750bb29b79ed961ab195cceea5': {
+      address: "0x2416092f143378750bb29b79ed961ab195cceea5",
+      name: "Renzo Restaked ETH",
+      symbol: "ezETH",
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/tokens/${'0x2416092f143378750bb29b79ed961ab195cceea5'}.png`
+    },
+    '0xca84812e477ee5a96a92328689d8ce2589ab6ffd': {
+      address: "0xca84812e477ee5a96a92328689d8ce2589ab6ffd",
+      name: "ALIEN",
+      symbol: "ALIEN",
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/tokens/${'0xca84812e477ee5a96a92328689d8ce2589ab6ffd'}.png`
+    },
+    '0x5fe8534a6f96cb01261bd96e98c17c2c1cab3204': {
+      address: "0x5fe8534a6f96cb01261bd96e98c17c2c1cab3204",
+      name: "Baja",
+      symbol: "BAJA",
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/tokens/${'0x5fe8534a6f96cb01261bd96e98c17c2c1cab3204'}.png`
+    },
+    '0x20fe91f17ec9080e3cac2d688b4ecb48c5ac3a9c': {
+      address: "0x20fe91f17ec9080e3cac2d688b4ecb48c5ac3a9c",
+      name: "YES",
+      symbol: "YES",
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/tokens/${'0x20fe91f17ec9080e3cac2d688b4ecb48c5ac3a9c'}.png`
+    },
     // points
     '0x0000000000000000000000000000000000000001': {
       address: "0x0000000000000000000000000000000000000001",
