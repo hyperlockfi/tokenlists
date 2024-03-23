7b0832575e1c4404cddb3e8e0b26a8103e771e39
0xeye <97349378+0xeye@users.noreply.github.com>
2024-03-23 21:48:04 +0000

    chore: update locations

--- src/tokenlists/hyperlock/overwrites.ts
+++ src/tokenlists/hyperlock/overwrites.ts
@@ -96,92 +96,92 @@ export const overwrites: OverwritesForList = {
       address: "0x0000000000000000000000000000000000000001",
       name: "JUICE",
       symbol: "JUICE",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/points/${'juice'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/points/${'juice'}.png`
     },
     '0x0000000000000000000000000000000000000002': {
       address: "0x0000000000000000000000000000000000000002",
       name: "METASTREET",
       symbol: "METASTREET",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/points/${'metastreet'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/points/${'metastreet'}.png`
     },
     '0x0000000000000000000000000000000000000003': {
       address: "0x0000000000000000000000000000000000000003",
       name: "THRUST",
       symbol: "THRUST",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/points/${'thruster'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/points/${'thruster'}.png`
     },
     '0x0000000000000000000000000000000000000004': {
       address: "0x0000000000000000000000000000000000000004",
       name: "BLAST",
       symbol: "BLAST",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/points/${'blast'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/points/${'blast'}.png`
     },
     '0x0000000000000000000000000000000000000005': {
       address: "0x0000000000000000000000000000000000000005",
       name: "HYPER",
       symbol: "HYPER",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/points/${'hyper'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/points/${'hyper'}.png`
     },
     '0x0000000000000000000000000000000000000006': {
       address: "0x0000000000000000000000000000000000000006",
       name: "RENZO",
       symbol: "RENZO",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/points/${'renzo'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/points/${'renzo'}.png`
     },
     '0x0000000000000000000000000000000000000007': {
       address: "0x0000000000000000000000000000000000000007",
       name: "EIGEN",
       symbol: "EIGEN",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/points/${'eigen'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/points/${'eigen'}.png`
     },
     // yield
     '0x4200000000000000000000000000000000000022': {
       address: "0x4200000000000000000000000000000000000022",
       name: "USDB-B",
       symbol: "USDB-B",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/yield/${'0x4200000000000000000000000000000000000022'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/yield/${'0x4200000000000000000000000000000000000022'}.png`
     },
     '0x2100d2da8d51269607cdc503e58242471c5d54e7': {
       address: "0x2100d2da8d51269607cdc503e58242471c5d54e7",
       name: "USDB-PART",
       symbol: "USDB-PART",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/yield/${'0x2100d2da8d51269607cdc503e58242471c5d54e7'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/yield/${'0x2100d2da8d51269607cdc503e58242471c5d54e7'}.png`
     },
     '0x0af63da91f2a50823863ab232253781cfd2b4695': {
       address: "0x0af63da91f2a50823863ab232253781cfd2b4695",
       name: "USDB-YOOT",
       symbol: "USDB-YOOT",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/yield/${'0x0af63da91f2a50823863ab232253781cfd2b4695'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/yield/${'0x0af63da91f2a50823863ab232253781cfd2b4695'}.png`
     },
     '0x52a53f25b2b717a02613a9259d3ed6a189b9d22a': {
       address: "0x52a53f25b2b717a02613a9259d3ed6a189b9d22a",
       name: "USDB-SWAY",
       symbol: "USDB-SWAY",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/yield/${'0x52a53f25b2b717a02613a9259d3ed6a189b9d22a'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/yield/${'0x52a53f25b2b717a02613a9259d3ed6a189b9d22a'}.png`
     },
     '0x4200000000000000000000000000000000000023': {
       address: "0x4200000000000000000000000000000000000023",
       name: "WETH-B",
       symbol: "WETH-B",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/yield/${'0x4200000000000000000000000000000000000023'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/yield/${'0x4200000000000000000000000000000000000023'}.png`
     },
     '0x0e402b19f5bb665e558f197db3ed1ffbb6369876': {
       address: "0x0e402b19f5bb665e558f197db3ed1ffbb6369876",
       name: "WETH-PART",
       symbol: "WETH-PART",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/yield/${'0x0e402b19f5bb665e558f197db3ed1ffbb6369876'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/yield/${'0x0e402b19f5bb665e558f197db3ed1ffbb6369876'}.png`
     },
     '0x6c07e20e618ac8b3f5f25b30d358ba62a458fd4d': {
       address: "0x6c07e20e618ac8b3f5f25b30d358ba62a458fd4d",
       name: "WETH-YOOT",
       symbol: "WETH-YOOT",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/yield/${'0x6c07e20e618ac8b3f5f25b30d358ba62a458fd4d'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/yield/${'0x6c07e20e618ac8b3f5f25b30d358ba62a458fd4d'}.png`
     },
     '0xa1dd7ee93b5990851fca04257232d7062fe9c315': {
       address: "0xa1dd7ee93b5990851fca04257232d7062fe9c315",
       name: "WETH-SWAY",
       symbol: "WETH-SWAY",
-      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/main/src/assets/images/yield/${'0xa1dd7ee93b5990851fca04257232d7062fe9c315'}.png`
+      logoURI: `https://raw.githubusercontent.com/hyperlockfi/tokenlists/feat/testnet/src/assets/images/yield/${'0xa1dd7ee93b5990851fca04257232d7062fe9c315'}.png`
     },
   },
 }
