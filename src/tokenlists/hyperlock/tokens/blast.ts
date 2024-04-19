49974f0cd3f00a8a17ad6e0e89f5ed2e7e9b2854
0xeye <97349378+0xeye@users.noreply.github.com>
2024-04-19 18:40:00 +0100

    chore: fix ezeth lpt

--- src/tokenlists/hyperlock/tokens/blast.ts
+++ src/tokenlists/hyperlock/tokens/blast.ts
@@ -27,6 +27,7 @@ export default [
   '0x93040d9775770a478370aea9f6048fe350e65544', // yield - ezeth:weth-pt 
   '0xb7543dc092d11aef167b42e1cc4aeb30c49d2e19', // yield - ezeth:weth-yt
   '0x65f28f49932181bd3cab189770f0e3dc850c57b5', // yield - ezeth:weth-sy
+  '0x92523143fc87ef79fd8a4461574fb0c07611d1fc', // yield - ezeth:weth-lpt
   // tokens
   '0x42e12d42b3d6c4a74a88a61063856756ea2db357', // orbit
   '0x216a5a1135a9dab49fa9ad865e0f22fe22b5630a', // pump
